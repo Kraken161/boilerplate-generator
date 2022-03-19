ce6c478a180ae1e70e0de5162d0b98880ec157cc
Kraken161 <66274761+Kraken161@users.noreply.github.com>
2022-03-19 09:12:16 +0100

    s

---
+++ cli/cli.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+const NodeBoilerPlate = require('../types/express/node-boilerplate')
+const yargs = require('yargs')
+
+const argv = yargs
+	.command('generate', 'Generates boilerplate.', {
+		generate: {
+			description: 'Boilerplate',
+			alias: 'gen',
+			type: 'string',
+		},
+	})
+	.option('express', {
+		alias: 'node',
+		description: 'Nodejs boilerplate',
+		type: 'boolean',
+	})
+	.help()
+	.alias('help', 'h').argv
+
+if (argv._.includes('generate')) {
+	if (argv.express) {
+		NodeBoilerPlate.createNodeBoilerplate()
+	}
+}
